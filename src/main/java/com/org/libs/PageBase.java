2ab008cf00fdc9f1d78218e078e76a23c866bc79
bhanu <bhanu.0911@gmail.com>
2021-02-07 13:15:19 +0530

    Added deals page to feature branch

---
+++ src/main/java/com/org/libs/PageBase.java
@@ -0,0 +1,5 @@
+package com.org.libs;
+
+public class PageBase {
+
+}
---
+++ src/main/java/com/org/pages/DealsPage.java
@@ -0,0 +1,11 @@
+package com.org.pages;
+
+import com.org.libs.PageBase;
+
+public class DealsPage  extends PageBase{
+	
+	DealsPage(){
+		System.out.println("Deal page constructor");
+	}
+
+}
